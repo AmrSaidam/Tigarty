6915ae97b647a8b62dd215e63b62a5fef13d0a01
AmrSaidam <a.amer.2008@hotmail.com>
2015-08-05 07:09:03 -0700

    set cookie in Reg

--- homer_admin-v1.7/Reg.php
+++ homer_admin-v1.7/Reg.php
@@ -16,7 +16,7 @@ function Insert_Into_DataBase()
     try {
         if (($connect = mysqli_connect($ServerName, $UserName, $Password, $DataBaseName))) {
             if ($Result = mysqli_query($connect, $InsertQuery)) {
-                echo 'connect successfully';
+                et_User_ID($_POST['UserName'] ,$connect);
             }
 
 
@@ -25,6 +25,21 @@ function Insert_Into_DataBase()
 
     } finally {
         $connect->close();
+    }
+
+
+}
+function Get_User_ID($UserNameFoSearch ,$connect)
+{
+    $SelectQuery = "SELECT AdminID from admin where username = '".$UserNameFoSearch."'";
+    if($result = $connect->query($SelectQuery))
+    {
+        while($row =  $result->fetch_assoc())
+        {
+            setcookie("UserName", $row['AdminID'], time() + (86400 * 30), "/");
+            throw new Exception("cannot retrieve the ID ");
+
+        }
     }
 
 
